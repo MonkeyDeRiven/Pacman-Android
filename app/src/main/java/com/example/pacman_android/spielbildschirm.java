59a1c347fbbcd45f0ea1998cc2bd104b008bb548
Mojtaba Zafar <Mojtaba.zafar@hotmail.com>
2022-01-19 19:11:01 +0100

    m,k

--- app/src/main/java/com/example/pacman_android/spielbildschirm.java
+++ app/src/main/java/com/example/pacman_android/spielbildschirm.java
@@ -2,7 +2,9 @@ package com.example.pacman_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +15,13 @@ import com.example.myfirstapp.R;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class spielbildschirm extends AppCompatActivity {
-int breite = 0;
+public class spielbildschirm extends AppCompatActivity   {
+
+
+    int breite = 0;
 int hoehe = 0;
 boolean check = false;
+
 
 public block[][] spielfeldarray;
 
@@ -28,36 +33,8 @@ public block[][] spielfeldarray;
 private int posX=0;
 private int posY=0;
 
-  public void fillPlayfield()
-  {
-      spielfeldarray = new block[breite/anzahlBloeckeBreite][hoehe/anzahlBloeckeHoehe];
-      int breiteBlock = breite/anzahlBloeckeBreite,hoeheBlock=hoehe/anzahlBloeckeHoehe;
-    ImageView spielfeld = findViewById(R.id.playfield);
 
-      for(int a=0,b=0,x=(int)spielfeld.getX(),y=(int)spielfeld.getY();y<hoehe;a++,x+=breiteBlock)
-      {block value = new block();
-      value.x1 = x;
-      value.x2 = x+breiteBlock;
-      value.y1 = y;
-      value.y2 = y+hoeheBlock;
-      value.inhalt = 1;
-      spielfeldarray[a][b]  = value;
 
-          if(a==anzahlBloeckeBreite-1){++b;a=-1;y+=hoeheBlock;x=-breiteBlock;}
-      }
-  }
-
-  public void playfield1()
-  {
-      for(int a=0;a<anzahlBloeckeBreite;a++)
-      {
-          spielfeldarray[a][0].inhalt =0 ;
-      }
-      for(int a=0;a<anzahlBloeckeBreite;a++)
-      {
-          spielfeldarray[a][anzahlBloeckeHoehe-1].inhalt =0 ;
-      }
-  }
 
 
 
@@ -65,17 +42,17 @@ private int posY=0;
     {
 
         ImageView player = findViewById(R.id.player);
-        if(direction == 0) player.setY(player.getY()-1);   //OBEN
-        else if(direction == 1) player.setX(player.getX()+1); //RECHTS
-        else if(direction == 2) player.setY(player.getY()+1); //UNTEN
-        else if(direction == 3) player.setX(player.getX()-1); //LINKS
+        if(direction == 0) player.setY(player.getY()-5);   //OBEN
+        else if(direction == 1) player.setX(player.getX()+5); //RECHTS
+        else if(direction == 2) player.setY(player.getY()+5); //UNTEN
+        else if(direction == 3) player.setX(player.getX()-5); //LINKS
         else if(direction == -1);                            //STEHEN BLEIBEN
     }
-public void checkPos() {
+/*public void checkPos() {
 
-    ImageView player = findViewById(R.id.player);
-    int x=(int)player.getX()+player.getWidth()/2;
-    int y = (int)player.getY()+player.getHeight()/2;
+     ImageView player = findViewById(R.id.player);
+    int x=(int)player.getX()+player.getWidth();
+    int y = (int)player.getY()+player.getHeight();
     for(int a =0,b=0;b!=anzahlBloeckeHoehe;a++)
     {
         if(x>=spielfeldarray[a][b].x1&&x<=spielfeldarray[a][b].x2&&
@@ -89,22 +66,23 @@ public void checkPos() {
             if(a==anzahlBloeckeBreite-1){++b;a=-1;}
     }
 
-    }
+    }*/
 
-  public void checkCollision()
-  {
+  public void checkCollision() {
+      ImageView player = findViewById(R.id.player);
+      int x =(int) player.getX();
+      int y =(int) player.getY();
+  //    if(direction == 0 && posY-1<0 ||y-5 == grenzen[0].y2 )direction = -1;
 
+      //    if(direction == 0 && posY-1<0 || spielfeldarray[posX][posY-1].inhalt==0)direction = -1;//OBEN
 
+      //     else  if(direction == 1 &&  posX>=anzahlBloeckeBreite|| spielfeldarray[posX+1][posY].inhalt==0)direction = -1;//RECHTS
+      //RECHTS
+      //   else if(direction == 2 &&  posY>=anzahlBloeckeHoehe)direction = -1;//UNTEN
+      //UNTEN
+//            else   if(direction == 3 &&  posX-1<0)direction = -1;}//LINKS
 
-               if(direction == 0 && posY-1<0 || spielfeldarray[posX][posY-1].inhalt==0)direction = -1;//OBEN
-
-             else  if(direction == 1 &&  posX>=anzahlBloeckeBreite|| spielfeldarray[posX+1][posY].inhalt==0)direction = -1;//RECHTS
-            //RECHTS
-             else if(direction == 2 &&  posY>=anzahlBloeckeHoehe|| spielfeldarray[posX][posY+1].inhalt==0)direction = -1;//UNTEN
-               //UNTEN
-            else   if(direction == 3 &&  posX-1<0 || spielfeldarray[posX-1][posY].inhalt==0)direction = -1;}//LINKS
-
-
+  }
 
 
 
@@ -118,29 +96,46 @@ public void checkPos() {
     protected void onResume() {
      ImageView player = findViewById(R.id.player);
       super.onResume();
-        Timer timer;
+        Timer timer2;
+        {
+            timer2 = new Timer();
+            timer2.scheduleAtFixedRate(new TimerTask() {
+                public void run() { // wird periodisch im Timer thread aufgerufen
+                    spielbildschirm.this.runOnUiThread(new Runnable() {
+                        public void run() {
+
+
+                        }
+                    });
+                }
+            }, 0, 5);
+
+
+
+      Timer timer;
            {
             timer = new Timer();
             timer.scheduleAtFixedRate(new TimerTask() {
                 public void run() { // wird periodisch im Timer thread aufgerufen
                     spielbildschirm.this.runOnUiThread(new Runnable() {
                         public void run() {
-
-                            if(check)checkPos();
-                            if(check)checkCollision();
-
                             move();
 
                         }
                     });
                 }
-            }, 0, 5);
+            }, 0, 20);
+
+
+
+
         }
 
 
 
 
 
+  }
   }
 
 
@@ -152,6 +147,7 @@ public void checkPos() {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.spielbildschirm);
+    //    hallo = findViewById(R.id.spielbildschirm2);
 
         Button up = findViewById(R.id.upb);
         Button left = findViewById(R.id.leftb);
@@ -198,12 +194,19 @@ public void checkPos() {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        ImageView spielfeld = findViewById(R.id.playfield);
-        breite = spielfeld.getWidth();
-        hoehe = spielfeld.getHeight();
-        fillPlayfield();
-        playfield1();
-        check = true;
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
     }
+
+
+
+
+
+
+
+
 
 }
